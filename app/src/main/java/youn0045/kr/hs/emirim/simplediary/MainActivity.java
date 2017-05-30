042cec4876f3a817fb653eb62a30835cefaa2bad
justhis01010101 <offentsive@naver.com>
2017-05-30 20:47:31 +0900

    현재 날짜 Datepicker에 설정과 날짜변경 핸들러

--- app/src/main/java/youn0045/kr/hs/emirim/simplediary/MainActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/simplediary/MainActivity.java
@@ -1,15 +1,19 @@
 package youn0045.kr.hs.emirim.simplediary;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     DatePicker date;
     EditText edit;
     Button but;
+    String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,24 @@ public class MainActivity extends AppCompatActivity {
         edit = (EditText) findViewById(R.id.edit);
         but = (Button) findViewById(R.id.but);
 
+        Calendar cal= Calendar.getInstance(); //new연산자로 생성 못하면 메서드 안에 현재 객체 참조값을 반환하도록
+        int year= cal.get(Calendar.YEAR);
+        int month= cal.get(Calendar.MONTH);
+        int day= cal.get(Calendar.DATE);
 
-
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                fileName = year+"_"+(month+1)+"_"+day+".txt";
+                String readData = readDiary(fileName);
+                edit.setText(readData);
+                but.setEnabled(true); //수정이나 새로 작성을 할 수 있다 활성화해서!
+            }
+        });
     }
+    public String readDiary(String fileName){
+
+        return null;
+    }
+
 }
